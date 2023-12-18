b66627ac9290896a131f1ce9b7519af79e084241
angelqtypie <angeljustineballaso07@gmail.com>
2023-12-17 17:44:27 -0800

    Adding style and js

---
+++ js/router.js
@@ -0,0 +1,9 @@
+
+//for typing effects
+var typingEffect = new Typed(".multiText",{
+  strings : ["coder", "web developer", "into joseph", "HAUAHUHAU"],
+  loop : true,
+  typeSpeed : 100,
+  backSpeed : 80,
+  backDelay : 1500
+});
