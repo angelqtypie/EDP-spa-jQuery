90629edba8b50d3e2a9fa4e24b42349e5e21be54
angelqtypie <angeljustineballaso07@gmail.com>
2023-12-18 15:22:49 -0800

    aadding my about page

--- js/router.js
+++ js/router.js
@@ -7,3 +7,35 @@ var typingEffect = new Typed(".multiText",{
   backSpeed : 80,
   backDelay : 1500
 });
+
+let sections = document.querySelectorAll('section');
+window.onscroll =() => {
+    sections.forEach(sec => { 
+let top = window.scrollY;
+let offset = sec.offsetTop -150;
+let height = sec.offsetHeight;
+
+if (top >= offset && top < offset + height) {
+    sec.classList.add('show-animate');
+}
+//if want to use repeating animatiom on scroll, use this
+else {
+    sec.classList.remove('show-animate');
+}
+    }
+)}
+
+//for smooth scroll on navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener('click',function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
